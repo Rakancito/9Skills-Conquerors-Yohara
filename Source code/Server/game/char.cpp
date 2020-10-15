fee8c53d2242eb3fd202bfb84b99848f31ba0e57
Robert <55710848+Rakancito@users.noreply.github.com>
2020-10-15 00:05:34 -0500

    Update char.cpp

--- Source code/Server/game/char.cpp
+++ Source code/Server/game/char.cpp
@@ -16,29 +16,3 @@
 	StopCheonunEvent();
 #endif
 
-//Search
-		default:
-			sys_err("CHARACTER::PointChange: %s: unknown point change type %d", GetName(), type);
-			return;
-			
-//add before
-
-#ifdef ENABLE_CONQUEROR_LEVEL
-		case POINT_INVINCIBLE:
-			SetPoint(type, amount);
-			val = GetPoint(type);
-			break;
-#endif
-		
-// Search
-
-		default:
-			sys_err("Unknown apply type %d name %s", bApplyType, GetName());
-			break;
-			
-// add before
-#ifdef ENABLE_CONQUEROR_LEVEL
-		case APPLY_INVINCIBLE:
-#endif
-			PointChange(aApplyInfo[bApplyType].bPointType, iVal);
-			break;
