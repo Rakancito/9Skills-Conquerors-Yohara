7fecf1483a629505825a7a04803ac644d9a82896
Robert <55710848+Rakancito@users.noreply.github.com>
2020-10-16 13:12:51 -0500

    Update char_skill.cpp

--- Source code/Server/game/char_skill.cpp
+++ Source code/Server/game/char_skill.cpp
@@ -31,7 +31,7 @@ if (GetSkillMasterType(dwSkillVnum) != SKILL_MASTER)
 #ifdef ENABLE_WOLFMAN_CHARACTER
 			if (GetSkillMasterType(pkSk->dwVnum) != SKILL_MASTER && (pkSk->dwVnum<= SKILL_FINISH | pkSk->dwVnum>= SKILL_ILIPUNGU))
 #else
-			if (GetSkillMasterType(pkSk->dwVnum) != SKILL_MASTER && (pkSk->dwVnum<= SKILL_FINISH | pkSk->dwVnum>= SKILL_ILIPUNGU))
+			if (GetSkillMasterType(pkSk->dwVnum) != SKILL_MASTER && (pkSk->dwVnum<= SKILL_FINISH | pkSk->dwVnum>= SKILL_CHEONUN))
 #endif
 #else
 			if (GetSkillMasterType(pkSk->dwVnum) != SKILL_MASTER)
